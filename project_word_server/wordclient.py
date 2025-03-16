705178a593635ae2d80076916d719468c7ba72c4
pandaec <ericchan1026@gmail.com>
2025-03-16 20:15:49 +0800

    Add alternative implementation of get packet

--- project_word_server/wordclient.py
+++ project_word_server/wordclient.py
@@ -36,6 +36,46 @@ def get_next_word_packet(s):
             return None
         packet_buffer = packet_buffer + data
 
+# Alternative implementation
+# def get_next_word_packet(s):
+#     """
+#     Return the next word packet from the stream.
+
+#     The word packet consists of the encoded word length followed by the
+#     UTF-8-encoded word.
+
+#     Returns None if there are no more words, i.e. the server has hung
+#     up.
+#     """
+
+#     global packet_buffer
+
+#     # First, try to get the word length (2 bytes)
+#     while len(packet_buffer) < WORD_LEN_SIZE:
+#         chunk = s.recv(1)
+#         if not chunk:  # Server hung up
+#             return None
+#         packet_buffer += chunk
+
+#     # Extract the word length
+#     word_len = int.from_bytes(packet_buffer[:WORD_LEN_SIZE], "big")
+
+#     # Now make sure we have the complete word
+#     while len(packet_buffer) < WORD_LEN_SIZE + word_len:
+#         chunk = s.recv(1)
+#         if not chunk:  # Server hung up
+#             return None
+#         packet_buffer += chunk
+
+#     # Extract the complete packet (length + word)
+#     word_packet = packet_buffer[:WORD_LEN_SIZE + word_len]
+
+#     # Update the buffer to remove the packet we just processed
+#     packet_buffer = packet_buffer[WORD_LEN_SIZE + word_len:]
+
+#     return word_packet
+
+
 
 def extract_word(word_packet):
     """
